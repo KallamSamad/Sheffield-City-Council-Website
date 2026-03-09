53b55e22816ef71083890e360cd2a731bb922ee3
Kallam <kallamsamad@gmail.com>
2026-03-09 10:18:53 +0000

    Added an Assets folder with an Image folder inside it. Added a simple nav bar. Refined the CSS with flexbox to match prototype

--- index.php
+++ index.php
@@ -3,13 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
+    <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
+        crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Sheffield</title>
 </head>
+<?php require_once "nav.php"?>
 <body>
-<div class="header"></div>
-<div class="middle"></div>
-<div class="bottom"></div>
+<div class="page"> 
+<div class="sandwich">    
+    <div class="header">
+        <img class="logo" src="Assets/Images/Sheffield.jpg" alt="Logo of website">
+        <h1>Welcome To Sheffield</h1>
+        <img class="logo" src="Assets/Images/Sheffield.jpg" alt="Logo of website" height="30px">
+    </div>
+
+     
+</div> 
 <?php require_once "footer.php"?>
+</div>
+ 
 </body>
 </html>
