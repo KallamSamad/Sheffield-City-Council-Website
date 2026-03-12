eee565ac4830cb158f0072aff0f8596bd3697c44
Kallam <kallamsamad@gmail.com>
2026-03-12 14:29:09 +0000

    Fixed broken image links on nav bar, used flex box to put text under icon, resized icons appropriately

--- nav.php
+++ nav.php
@@ -3,22 +3,22 @@
 
 
 <li class="nav-item">
-<a class="nav-link text-white" href="index.php" img src= "Assets/Images/home.svg">Home</a>
+<a class="nav-link text-white" href="index.php"><img class="icon" src= "Assets/Images/home.svg"  height="60px" width="60px">Home</a>
 </li>
 
 <li class="nav-item">
-<a class="nav-link text-white" href="#"img src= "Assets/Images/map.svg">Map</a>
+<a class="nav-link text-white" href="#"><img class="icon" src= "Assets/Images/map.svg"  height="60px" width="60px" >Map</a>
 </li>
 
 <li class="nav-item">
-<a class="nav-link text-white" href="#" img src= "Assets/Images/contact.svg">Contact</a>
+<a class="nav-link text-white" href="#" ><img class="icon" src= "Assets/Images/contact.svg"  height="60px" width="60px" >Contact</a>
 </li>
 <li class="nav-item">
-<a class="nav-link text-white" href="#"img src= "Assets/Images/transport.svg">Transport Links</a>
+<a class="nav-link text-white" href="#"><img class="icon" src= "Assets/Images/transport.svg"  height="60px" width="60px" >Transport Links</a>
 </li>
 
 <li class="nav-item">
-<a class="nav-link text-white" href="#"img src= "Assets/Images/education.svg">Education</a>
+<a class="nav-link text-white" href="#"><img  class="icon"src= "Assets/Images/education.svg" height="60px" width="60px">Education</a>
 </li>
 
  
