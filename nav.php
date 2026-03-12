1621e99b02af7c879ffbbccb04bc18edc1e1e078
Kallam <kallamsamad@gmail.com>
2026-03-12 14:05:28 +0000

    Save local changes before merging main

--- nav.php
+++ nav.php
@@ -3,22 +3,22 @@
 
 
 <li class="nav-item">
-<a class="nav-link text-white" href="index.php">Home</a>
+<a class="nav-link text-white" href="index.php" img src= "Assets/Images/home.svg">Home</a>
 </li>
 
 <li class="nav-item">
-<a class="nav-link text-white" href="#">Map</a>
+<a class="nav-link text-white" href="#"img src= "Assets/Images/map.svg">Map</a>
 </li>
 
 <li class="nav-item">
-<a class="nav-link text-white" href="#">Contact</a>
+<a class="nav-link text-white" href="#" img src= "Assets/Images/contact.svg">Contact</a>
 </li>
 <li class="nav-item">
-<a class="nav-link text-white" href="#">Transport Links</a>
+<a class="nav-link text-white" href="#"img src= "Assets/Images/transport.svg">Transport Links</a>
 </li>
 
 <li class="nav-item">
-<a class="nav-link text-white" href="#">Education</a>
+<a class="nav-link text-white" href="#"img src= "Assets/Images/education.svg">Education</a>
 </li>
 
  
