d2f5005fb3878a98eb7f991567e3e653473fbf6f
Kallam <kallamsamad@gmail.com>
2026-03-18 15:54:22 +0000

    bootstrap

--- Contact.php
+++ Contact.php
@@ -2,14 +2,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact</title>
-    <link rel="stylesheet" href="style.css">
-</head>
 
+    <link rel="stylesheet" href="style.css">
+    <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous">
+</head>
 <body>
+
 <?php require_once 'nav.php'; ?>
 
-<!-- Contact Content -->
 <div class="page">
     <h1>Contact Us</h1>
 
@@ -37,5 +43,6 @@
 
 <?php require_once "footer.php"; ?>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
