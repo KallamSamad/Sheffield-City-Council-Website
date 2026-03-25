513859e245b6b3d9f35145ad22f135e024107a6b
Kallam <kallamsamad@gmail.com>
2026-03-25 04:01:47 +0000

    Added student gallery and will hide initially

--- gallery.php
+++ gallery.php
@@ -16,19 +16,35 @@
 <body>
 <div class="sandwich"> 
     <div class="grid">
-    <div class="album"><button class="albumbtn"><image src="Assets/Images/photo-album/student/SHU/sign.webp" height="200px">Student Album</button></div>
-    <div class="album"><button class="albumbtn"><image src="Assets/Images/photo-album/nature/peacegarden.webp" height="200px">Nature Album</button></div>
-    <div class="album"><button class="albumbtn"><image src="Assets/Images/photo-album/city/parkhill.webp" height="200px">City Album</button></div>
-    <div class="album"><button class="albumbtn"><div class="playcont"> <image class="play" src="Assets/Images/play.svg"><image src="Assets/Images/photo-album/video/video.png" style="fill: maroon;" height="200px" width="300px"></div>Video Album</button></div>
+    <div class="album1"><button class="albumbtn"><img src="Assets/Images/photo-album/student/SHU/sign.webp" height="200px">Student Album</button></div>
+    <div class="album2"><button class="albumbtn"><img src="Assets/Images/photo-album/nature/peacegarden.webp" height="200px">Nature Album</button></div>
+    <div class="album3"><button class="albumbtn"><img src="Assets/Images/photo-album/city/parkhill.webp" height="200px">City Album</button></div>
+    <div class="album4"><button class="albumbtn"><div class="playcont"> <img class="play" src="Assets/Images/play.svg"><img src="Assets/Images/photo-album/video/video.png" style="fill: maroon;" height="200px" width="300px"></div>Video Album</button></div>
  
+
+    <div class="studentalbum" style="display:block">
+    <img class="studentimg" src="Assets/Images/photo-album/student/SHU/sign.webp">
+    <img class="studentimg" src="Assets/Images/photo-album/student/SHU/owen.webp">
+    <img class="studentimg" src="Assets/Images/photo-album/student/SHU/owen2.webp">
+    <img class="studentimg" src="Assets/Images/photo-album/student/SHU/poem.webp">
+    <img class="studentimg" src="Assets/Images/photo-album/student/UoS/campus.webp">
+    <img class="studentimg" src="Assets/Images/photo-album/student/UoS/latin.webp">
+    <img class="studentimg" src="Assets/Images/photo-album/student/UoS/sign.webp">
+    <img class="studentimg" src="Assets/Images/photo-album/student/UoS/uosbuild.webp">
+    </div>
+
 </div>
 </div>
  <?php require_once "footer.php" ?>
 <script>
+    
     const grid =document.querySelector(".grid");
+    
+
+
     grid.addEventListener("click",function (){
     grid.style.display="none";
-
+    
 
     })
 
