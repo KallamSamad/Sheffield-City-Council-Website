03062c341d21aebc5bf4957515786cbf3b36e9f3
Kallam <kallamsamad@gmail.com>
2026-03-25 04:34:24 +0000

    Added all albums including video, made display none now to do JS

--- gallery.php
+++ gallery.php
@@ -16,13 +16,13 @@
 <body>
 <div class="sandwich"> 
     <div class="grid">
-    <div class="album1"><button class="albumbtn"><img src="Assets/Images/photo-album/student/SHU/sign.webp" height="200px">Student Album</button></div>
-    <div class="album2"><button class="albumbtn"><img src="Assets/Images/photo-album/nature/peacegarden.webp" height="200px">Nature Album</button></div>
-    <div class="album3"><button class="albumbtn"><img src="Assets/Images/photo-album/city/parkhill.webp" height="200px">City Album</button></div>
-    <div class="album4"><button class="albumbtn"><div class="playcont"> <img class="play" src="Assets/Images/play.svg"><img src="Assets/Images/photo-album/video/video.png" style="fill: maroon;" height="200px" width="300px"></div>Video Album</button></div>
- 
+    <div class="album1"><button class="albumbtn"><img class="imground" src="Assets/Images/photo-album/student/SHU/sign.webp" height="200px">Student Album</button></div>
+    <div class="album2"><button class="albumbtn"><img class="imground"  src="Assets/Images/photo-album/nature/peacegarden.webp" height="200px">Nature Album</button></div>
+    <div class="album3"><button class="albumbtn"><img class="imground"  src="Assets/Images/photo-album/city/parkhill.webp" height="200px">City Album</button></div>
+    <div class="album4"><button class="albumbtn"><div class="playcont"> <img class="play" src="Assets/Images/play.svg"><img class="imground" src="Assets/Images/photo-album/video/video.png" style="fill: maroon;" height="200px" width="300px"></div>Video Album</button></div>
+    </div>
 
-    <div class="studentalbum" style="display:block">
+    <div class="studentalbum" style="display:none">
     <img class="studentimg" src="Assets/Images/photo-album/student/SHU/sign.webp">
     <img class="studentimg" src="Assets/Images/photo-album/student/SHU/owen.webp">
     <img class="studentimg" src="Assets/Images/photo-album/student/SHU/owen2.webp">
@@ -33,8 +33,39 @@
     <img class="studentimg" src="Assets/Images/photo-album/student/UoS/uosbuild.webp">
     </div>
 
-</div>
-</div>
+
+    <div class="cityalbum" style="display:none">
+    <img class="cityimg" src="Assets/Images/photo-album/city/center.webp">
+    <img class="cityimg" src="Assets/Images/photo-album/city/lightup.webp">
+    <img class="cityimg" src="Assets/Images/photo-album/city/lightup2.webp">
+    <img class="cityimg" src="Assets/Images/photo-album/city/parkhill.webp">
+    <img class="cityimg" src="Assets/Images/arial.jpg">
+    <img class="cityimg" src="Assets/Images/sheffield.jpg">
+    <img class="cityimg" src="Assets/Images/xmasmark1.jpg">
+    </div>
+
+
+
+        <div class="naturealbum" style="display:none">
+    <img class="studentimg" src="Assets/Images/photo-album/nature/norfolkheritage.webp">
+    <img class="studentimg" src="Assets/Images/photo-album/nature/peacegarden.webp">
+    <img class="studentimg" src="Assets/Images/photo-album/nature/rivelin.webp">
+    <img class="studentimg" src="Assets/Images/photo-album/nature/sheffpark.webp">
+    <img class="studentimg" src="Assets/Images/peakdistrict.jpg">
+
+    </div>
+
+
+        <div class="videoalbum" style="display:none">
+        <video class="studentimg" controls><source src="Assets/Images/video/1.mp4" type="video/mp4"></source></video>
+        <video class="studentimg" controls><source src="Assets/Images/video/2.mp4" type="video/mp4" ></source></video>
+        <video class="studentimg" controls><source src="Assets/Images/video/3.mp4" type="video/mp4"></source></video>
+        <video class="studentimg" controls><source src="Assets/Images/video/4.mp4" type="video/mp4"></source></video>
+        <video class="studentimg" controls><source src="Assets/Images/video/5.mp4" type="video/mp4"></source></video>
+        <video class="studentimg" controls> <source src="Assets/Images/video/6.mp4" type="video/mp4"></source></video>
+        <video class="studentimg "controls><source src="Assets/Images/video/7.mp4" type="video/mp4"></source></video>
+    </div>
+    </div>
  <?php require_once "footer.php" ?>
 <script>
     
